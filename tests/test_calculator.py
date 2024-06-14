c43d37347210a8328e2686171795c9fbab675e1e
NJIT-NSD <nsd35@njit.edu>
2024-06-14 01:33:00 -0400

    added additional tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,25 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import addition, subtraction, multiplication, division
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert addition(2,2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
-    
+    '''Test that subtraction function works '''    
+    assert subtraction(2,2) == 0
+
+def test_multiplication():
+    '''Test that multplication function works '''
+    assert multiplication(3,3) == 9
+
+def test_division():
+    '''Test that division function works '''
+    assert division(9, 3) == 3
+
+def test_divide_by_zero():
+    '''Test that divide by zero error occurs '''
+    assert division(5, 0) == 1
+
+#def history_operations():
+#    '''Check History'''
