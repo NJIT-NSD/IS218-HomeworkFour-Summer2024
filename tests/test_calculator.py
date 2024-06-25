b4b90d5977cbaf5cb609af73e66d28af9a447d9d
NJIT-NSD <nsd35@njit.edu>
2024-06-25 12:18:25 -0400

    corrected attribute names

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,25 +1,25 @@
 '''My Calculator Test'''
-from calculator import addition, subtraction, multiplication, division
+from calculator.calculator_section import Calculations
 
 def test_addition():
     '''Test that addition function works '''    
-    assert addition(2,2) == 4
+    assert Calculations.addition(2,2) == 4
 
 def test_subtraction():
     '''Test that subtraction function works '''    
-    assert subtraction(2,2) == 0
+    assert Calculations.subtraction(2,2) == 0
 
 def test_multiplication():
     '''Test that multplication function works '''
-    assert multiplication(3,3) == 9
+    assert Calculations.multiplication(3,3) == 9
 
 def test_division():
     '''Test that division function works '''
-    assert division(9, 3) == 3
+    assert Calculations.division(9, 3) == 3
 
 def test_divide_by_zero():
     '''Test that divide by zero error occurs '''
-    assert division(5, 0) == 1
+    assert Calculations.division(5, 0) == 1
 
 #def history_operations():
 #    '''Check History'''
